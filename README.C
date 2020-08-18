f624a3596b803e3b7c9d9be6967c30b9a6eea81c
Priyadebnath <69429279+Priyadebnath@users.noreply.github.com>
2020-08-18 15:48:52 +0530

    README1.C
    To check if a given number is a power of 2 or not.

--- README.C
+++ README.C
@@ -1,21 +1,14 @@
 #include<stdio.h>
 int powerofTwo(int n)
 {
-  printf("ENTER THE NUMBER n");
-  scanf("%d",&n);
-  if(n == 0)
-  {
-    printf("THE GIVEN NUMBER n IS NOT A POWER OF 2");
-    return 0;
-  }
-  while (n != 1)
-  {
-   if(n%2==0)
-   printf("THE GIVEN NUMBER n IS A POWER OF 2");
-   else
-   printf("THE GIVEN NUMBER n IS NOT A POWER OF 2");
-   }
+printf("ENTER THE NUMBER n");
+scanf("%d",&n);
+{
+if(n == 0)
+  return 0;
+if(n&(n-1)==0)
+  printf("n IS THE POWER OF 2");
+else
+  printf("n IS NOT THE POWER OF 2");
 }
 
-
- 
